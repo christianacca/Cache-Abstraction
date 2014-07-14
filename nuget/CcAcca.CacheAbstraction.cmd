1ad7102d79ac9e932841a24fd86a440533271908
christianacca <ghp@christianacca.e4ward.com>
2014-07-14 22:45:07 +0100

    Choir: Simplified nuget publishing script

--- nuget/CcAcca.CacheAbstraction.cmd
+++ nuget/CcAcca.CacheAbstraction.cmd
@@ -1,7 +1,2 @@
-mkdir ..\dist\CcAcca.CacheAbstraction\lib\net40
-xcopy ..\bin\Release\CcAcca.CacheAbstraction\CcAcca.CacheAbstraction.dll ..\dist\CcAcca.CacheAbstraction\lib\net40 /y
-xcopy ..\bin\Release\CcAcca.CacheAbstraction\CcAcca.CacheAbstraction.pdb ..\dist\CcAcca.CacheAbstraction\lib\net40 /y
-xcopy ..\bin\Release\CcAcca.CacheAbstraction\CcAcca.CacheAbstraction.XML ..\dist\CcAcca.CacheAbstraction\lib\net40 /y
-xcopy  CcAcca.CacheAbstraction\* ..\dist\CcAcca.CacheAbstraction /s /i
-NuGet.exe pack ..\dist\CcAcca.CacheAbstraction\CcAcca.CacheAbstraction.nuspec -OutputDirectory ..\dist
-rmdir /s /q ..\dist\CcAcca.CacheAbstraction
+mkdir ..\dist
+NuGet.exe pack CcAcca.CacheAbstraction\CcAcca.CacheAbstraction.nuspec -OutputDirectory ..\dist -Symbols
