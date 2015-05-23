58281861716be3deaf8637dd73d09ea12c136443
ccrowhurstram <ccrowhurst@ramplc.com>
2015-05-23 17:09:04 +0100

    chore(release): 2.0.0

--- src/CcAcca.CacheAbstraction.Distributed/Properties/AssemblyInfo.cs
+++ src/CcAcca.CacheAbstraction.Distributed/Properties/AssemblyInfo.cs
@@ -6,8 +6,8 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CcAcca.CacheAbstraction.Distributed")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("Simple yet extensible cache abstraction for the .net framework. This package adds an adaptor for https://github.com/MichaCo/CacheManager library and thus add support for distributed caches such as Redis, Memcached and Couchbase.")]
+[assembly: AssemblyDescription("Simple yet extensible cache abstraction for the .net framework. This package adds an adaptor for https://github.com/MichaCo/CacheManager library and thus add support for distributed caches such as Redis, Memcached and Couchbase.")]
+[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CcAcca")]
 [assembly: AssemblyProduct("CcAcca.CacheAbstraction.Distributed")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
@@ -34,3 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.0")]
